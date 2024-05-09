38f93be1291ab6a690a8f8454c88f39642cef2d6
shrutiVishwakarma05 <shrutivishwakarma0509@gmail.com>
2024-05-09 16:51:46 +0530

    docsadded

---
+++ my-app/src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import Home from "@/modules/home";
+
+export default function Page() {
+  return (
+   <div>
+    <Home />
+   </div>
+  );
+}
---
+++ my-app/src/modules/home.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div className='w-full sticky'>
+        
+    </div>
+  )
+}
+
+export default Home
